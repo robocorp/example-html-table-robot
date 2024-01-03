aeaa8bd2a43a85a9310bbbf1500d6f4bc631bf0e
Matti Mokko <mmokko@users.noreply.github.com>
2024-01-03 13:54:01 +0200

    Convert to python using robocorp library (#4)

--- html_tables.py
+++ html_tables.py
@@ -1,17 +1,30 @@
 from bs4 import BeautifulSoup
-from RPA.Tables import Table
+from dataclasses import dataclass
+from typing import Tuple
 
+@dataclass
+class Table:
+    headers: list[str]
+    rows: [list[list[str]]]
+
+    def dimensions(self) -> Tuple[int, int]:
+        return (0 if len(self.rows) == 0 else len(self.rows[0]), len(self.rows))
 
 def read_table_from_html(html_table: str) -> Table:
     """Parses and returns the given HTML table as a Table structure.
 
     :param html_table: Table HTML markup.
     """
+    table_headers = []
     table_rows = []
     soup = BeautifulSoup(html_table, "html.parser")
 
     for table_row in soup.select('tr'):
+        headers = table_row.find_all('th')
         cells = table_row.find_all('td')
+
+        if headers:
+            table_headers.append([header.text.strip() for header in headers])
 
         if len(cells) > 0:
             cell_values = []
@@ -21,4 +34,4 @@ def read_table_from_html(html_table: str) -> Table:
 
             table_rows.append(cell_values)
 
-    return Table(table_rows)
+    return Table(headers=table_headers, rows=table_rows)
